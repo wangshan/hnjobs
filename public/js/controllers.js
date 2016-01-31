5889c61e58a503b72473948c7707de4796df58ea
Shan Wang <uniwangshan@gmail.com>
2016-01-31 20:25:33 +0000

    Add a asHtml filter to show html content in job text

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,139 +1,145 @@
 'use strict';
 
-angular.module('confusionApp')
+var app = angular.module('confusionApp');
 
-        .controller('HnJobsController', ['$scope', 'hnJobsFactory', function($scope, hnJobsFactory) {
-            
-            $scope.tab = 1;
-            $scope.filtText = '';
-            $scope.showHnJobs = false;
-            $scope.message = "Loading ...";
+app.filter('asHtml', ['$sce', function($sce) {
+    return function(raw) {
+        return $sce.trustAsHtml(raw);
+    };
+}]);
 
-            $scope.jobs = hnJobsFactory.getHnJobs().query(
-                function(response) { // the response is the actual data
-                    $scope.jobs = response;
-                    $scope.showHnJobs = true;
-                },
-                function(response) { // but here is the response object, why?
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-                );
- 
-            $scope.select = function(setTab) {
-                $scope.tab = setTab;
+app.controller('HnJobsController', ['$scope', 'hnJobsFactory', function($scope, hnJobsFactory) {
+    
+    $scope.tab = 1;
+    $scope.filtText = '';
+    $scope.showHnJobs = false;
+    $scope.message = "Loading ...";
+
+    $scope.jobs = hnJobsFactory.getHnJobs().query(
+        function(response) { // the response is the actual data
+            $scope.jobs = response;
+            $scope.showHnJobs = true;
+        },
+        function(response) { // but here is the response object, why?
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        }
+        );
+
+    $scope.select = function(setTab) {
+        $scope.tab = setTab;
+        
+        if (setTab === 2) {
+            $scope.filtText = "January 2016";
+        }
+        else if (setTab === 3) {
+            $scope.filtText = "December 2015";
+        }
+        else if (setTab === 4) {
+            $scope.filtText = "November 2015";
+        }
+        else {
+            $scope.filtText = "";
+        }
+    };
+
+    $scope.isSelected = function (checkTab) {
+        return ($scope.tab === checkTab);
+    };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+
+    $scope.feedback = { mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
                 
-                if (setTab === 2) {
-                    $scope.filtText = "January 2016";
-                }
-                else if (setTab === 3) {
-                    $scope.filtText = "December 2015";
-                }
-                else if (setTab === 4) {
-                    $scope.filtText = "November 2015";
-                }
-                else {
-                    $scope.filtText = "";
-                }
-            };
+}])
 
-            $scope.isSelected = function (checkTab) {
-                return ($scope.tab === checkTab);
-            };
-        }])
-
-        .controller('ContactController', ['$scope', function($scope) {
-
-            $scope.feedback = { mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            
-            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-            
-            $scope.channels = channels;
+.controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
+    
+    $scope.sendFeedback = function() {
+        
+        console.log($scope.feedback);
+        
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        }
+        else {
             $scope.invalidChannelSelection = false;
-                        
-        }])
+            feedbackFactory.getFeedback().save($scope.feedback);
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback.mychannel="";
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
+}])
 
-        .controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
-            
-            $scope.sendFeedback = function() {
-                
-                console.log($scope.feedback);
-                
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-                    $scope.invalidChannelSelection = true;
-                    console.log('incorrect');
-                }
-                else {
-                    $scope.invalidChannelSelection = false;
-                    feedbackFactory.getFeedback().save($scope.feedback);
-                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                    $scope.feedback.mychannel="";
-                    $scope.feedbackForm.$setPristine();
-                    console.log($scope.feedback);
-                }
-            };
-        }])
+.controller('DishCommentController', ['$scope', 'hnJobsFactory', function($scope, hnJobsFactory) {
+    
+    //Step 1: Create a JavaScript object to hold the comment from the form
+    $scope.newComment = {
+        rating: 5,
+        comment: "",
+        author: "",
+        date: ""
+    };
+    
+    $scope.submitComment = function () {
+        
+        //Step 2: This is how you record the date
+        $scope.newComment.date = new Date().toISOString();
 
-        .controller('DishCommentController', ['$scope', 'hnJobsFactory', function($scope, hnJobsFactory) {
-            
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.newComment = {
-                rating: 5,
-                comment: "",
-                author: "",
-                date: ""
-            };
-            
-            $scope.submitComment = function () {
-                
-                //Step 2: This is how you record the date
-                $scope.newComment.date = new Date().toISOString();
+        console.log($scope.newComment);
+        
+        // Step 3: Push your comment into the dish's comment array
+        $scope.dish.comments.push($scope.newComment);
+        hnJobsFactory.getHnJobs().update({id: $scope.dish.id}, $scope.dish);
+        
+        //Step 4: reset your form to pristine
+        $scope.commentForm.$setPristine();
+        
+        //Step 5: reset your JavaScript object that holds your comment
+        $scope.newComment = {
+            rating: 5,
+            comment: "",
+            author: "",
+            date: ""
+        };
+    }
+}])
 
-                console.log($scope.newComment);
-                
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push($scope.newComment);
-                hnJobsFactory.getHnJobs().update({id: $scope.dish.id}, $scope.dish);
-                
-                //Step 4: reset your form to pristine
-                $scope.commentForm.$setPristine();
-                
-                //Step 5: reset your JavaScript object that holds your comment
-                $scope.newComment = {
-                    rating: 5,
-                    comment: "",
-                    author: "",
-                    date: ""
-                };
-            }
-        }])
+.controller('AboutController', ['$scope', '$stateParams', 'corporateFactory', function($scope, $stateParams, corporateFactory) {
 
-        .controller('AboutController', ['$scope', '$stateParams', 'corporateFactory', function($scope, $stateParams, corporateFactory) {
+    $scope.showLeaders = false;
+    $scope.showLeader = false;
+    $scope.message = "Loading ...";
 
-            $scope.showLeaders = false;
-            $scope.showLeader = false;
-            $scope.message = "Loading ...";
+    $scope.leaders = corporateFactory.getLeaders().query(
+        function(response) { // the response is the actual data
+            $scope.leaders = response;
+            $scope.showLeaders = true;
+        },
+        function(response) { // but here is the response object, why?
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        }
+    );
 
-            $scope.leaders = corporateFactory.getLeaders().query(
-                function(response) { // the response is the actual data
-                    $scope.leaders = response;
-                    $scope.showLeaders = true;
-                },
-                function(response) { // but here is the response object, why?
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
-            $scope.leader = corporateFactory.getLeaders().get({id: parseInt($stateParams.id, 10)})
-            .$promise.then(
-                function(response) {
-                    $scope.leader = response;
-                    $scope.showLeader = true;
-                },
-                function(response) {
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-            
-        }])
+    $scope.leader = corporateFactory.getLeaders().get({id: parseInt($stateParams.id, 10)})
+    .$promise.then(
+        function(response) {
+            $scope.leader = response;
+            $scope.showLeader = true;
+        },
+        function(response) {
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        }
+    );
+    
+}])
 
 ;
