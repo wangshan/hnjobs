5f55e6e048a95c0ca22d113ea058fdd314f1913b
Shan Wang <uniwangshan@gmail.com>
2016-02-05 18:39:15 +0000

    Rmoved some useless html

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -81,11 +81,13 @@ app.controller('HnJobsController',
 
 .controller('ContactController', ['$scope', function($scope) {
 
-    $scope.feedback = { mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    $scope.feedback = {
+        firstName: "",
+        lastName: "",
+        agree: false,
+        email: "" 
+    };
     
-    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-    
-    $scope.channels = channels;
     $scope.invalidChannelSelection = false;
                 
 }])
@@ -96,81 +98,22 @@ app.controller('HnJobsController',
         
         console.log($scope.feedback);
         
-        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+        if ($scope.feedback.agree && ($scope.feedback.email == "")) {
             $scope.invalidChannelSelection = true;
             console.log('incorrect');
         }
         else {
             $scope.invalidChannelSelection = false;
             feedbackFactory.getFeedback().save($scope.feedback);
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            $scope.feedback.mychannel="";
+            $scope.feedback = { firstName: "", lastName: "", agree: false, email: "" };
+            $scope.feedback.email = "";
             $scope.feedbackForm.$setPristine();
             console.log($scope.feedback);
         }
     };
 }])
 
-.controller('DishCommentController', ['$scope', 'hnJobsFactory', function($scope, hnJobsFactory) {
-    
-    //Step 1: Create a JavaScript object to hold the comment from the form
-    $scope.newComment = {
-        rating: 5,
-        comment: "",
-        author: "",
-        date: ""
-    };
-    
-    $scope.submitComment = function () {
-        
-        //Step 2: This is how you record the date
-        $scope.newComment.date = new Date().toISOString();
-
-        console.log($scope.newComment);
-        
-        // Step 3: Push your comment into the dish's comment array
-        $scope.dish.comments.push($scope.newComment);
-        hnJobsFactory.getHnJobs().update({id: $scope.dish.id}, $scope.dish);
-        
-        //Step 4: reset your form to pristine
-        $scope.commentForm.$setPristine();
-        
-        //Step 5: reset your JavaScript object that holds your comment
-        $scope.newComment = {
-            rating: 5,
-            comment: "",
-            author: "",
-            date: ""
-        };
-    }
-}])
-
-.controller('AboutController', ['$scope', '$stateParams', 'corporateFactory', function($scope, $stateParams, corporateFactory) {
-
-    $scope.showLeaders = false;
-    $scope.showLeader = false;
-    $scope.message = "Loading ...";
-
-    $scope.leaders = corporateFactory.getLeaders().query(
-        function(response) { // the response is the actual data
-            $scope.leaders = response;
-            $scope.showLeaders = true;
-        },
-        function(response) { // but here is the response object, why?
-            $scope.message = "Error: " + response.status + " " + response.statusText;
-        }
-    );
-
-    $scope.leader = corporateFactory.getLeaders().get({id: parseInt($stateParams.id, 10)})
-    .$promise.then(
-        function(response) {
-            $scope.leader = response;
-            $scope.showLeader = true;
-        },
-        function(response) {
-            $scope.message = "Error: " + response.status + " " + response.statusText;
-        }
-    );
+.controller('AboutController', ['$scope', '$stateParams', function($scope, $stateParams) {
     
 }])
 
--- public/js/services.js
+++ public/js/services.js
@@ -13,17 +13,13 @@ angular.module('confusionApp')
                 // convert response.data to an array of Date
                 var dates = [];
                 var data = response.data;
-//                console.log("parseResponseDates, data.length=" + data.length);
                 var key;
                 var value;
                 for (key in data) {
-//                    console.log("parseResponseDates, raw=", data[key]);
                     if (!data.hasOwnProperty(key) && // don't parse prototype or non-string props
                         toString.call(data[key]) !== '[object String]') continue;
                     value = Date.parse(data[key].month); // try to parse to date
                     if (value !== NaN) {
-//                        console.log("parseResponseDates, parsed=", value);
-                        //data[key] = value;
                         dates.push(new Date(value));
                     }
                 }
@@ -45,16 +41,12 @@ angular.module('confusionApp')
             };
         }])
 
-        .service('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
-            this.getLeaders = function() {
-                return $resource(baseURL + "leadership/:id", null, {'update':{method: 'PUT'}}); 
-            };
-        }])
-
         .service('feedbackFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+            /*
             this.getFeedback = function() {
                 return $resource(baseURL + "feedback/:id", null, {'save':{method: 'POST'}}); 
             }
+            */
         }])
 
 ;
