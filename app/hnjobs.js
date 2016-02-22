1038b8e612994d594e9a65e93026c157bf6260a1
Shan Wang <uniwangshan@gmail.com>
2016-02-22 18:41:08 +0000

    Rename some files

--- app/hnjobs.js
+++ app/hnjobs.js
@@ -1,265 +1,162 @@
-var https = require("https");
-var fs = require("fs");
+var HnJobs = require('./requester');
+var DateLabel = require('./models/date_label');
+var CronJob = require('cron').CronJob;
 var _ = require('lodash');
-var JobDatum = require('./models/job');
-var CandidateDatum = require('./models/candidate');
+var MonthYear = require('./month_year');
 
-var requestById = function(type, id, onEnd) {
-    var options = {
-        hostname: "hacker-news.firebaseio.com",
-        path: "/v0/" + type + id + ".json?print=pretty",
-        method: "GET",
-        headers: { Accept: "text/html" }
-    };
+var argv = require('yargs')
+    .usage('Usage: node $0 [options]')
+    .help('h')
+    .alias('h', 'help')
+    .boolean(['f', 'd'])
+    .alias('f', 'force')
+    .describe('f', 'Force refresh database')
+    .default('f', false)
+    .alias('d', 'debug')
+    .describe('d', 'Run in debug mode')
+    .default('d', false)
+    .demand(['m'])
+    .alias('m', 'mode')
+    .choices(['oneoff', 'daemon'])
+    .describe('m', 'Running mode, daemon mode will refresh database regularly')
+    .default('m', 'oneoff')
+    .argv;
+ 
+console.log("starting...");
 
-    var request = https.request(options,
-        function(response) {
-            //console.log("Server responded with status code: ", response.statusCode);
-            //console.log("Server responded with header: ", response.headers);
-            var body = "";
-            response.on("data", function(chunk) {
-                body += chunk;
-            });
-            response.on('end', function() {
-                var content = JSON.parse(body);
-                onEnd(content);
-            });
-        });
-    request.end();
-
-    request.on("error", function(e) {
-        console.error("request has error: " + e);
-    });
-}
-
-var topics = [
-    { title: "hiring"     , text: "Ask HN: Who is hiring.*" },
-    { title: "wantshired" , text: "Ask HN: Who wants to be hired.*" },
-    { title: "freelance"  , text: "Ask HN: Freelancer.*" },
+var getMonthName = function(date) {
+    var monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ];
+    return monthNames[date.getMonth()];
+}
 
-var parseWhosHiring = function(fileName, data, filter) {
-    var foundMatch = false;
-    if (data.title) {
-        for (var i = 0; i < topics.length; ++i) {
-            var topic = topics[i];
-            var re = new RegExp(topic.text + filter, "gi");
-            if (re.test(data.title)) {
-                data.kids.forEach(function(entry) {
-                    //console.log("requesting item/" + entry);
-                    requestById("item/", entry, function(post) {
-                        if (post) {
-                            savePostToDatabase(post, topic, filter);
-                        }
-                    });
-                });
-                foundMatch = true;
-                break;
+var getMonthYearText = function(date) {
+    if (!_.isDate(date)) {
+        console.log("getMonthYearText, parameter is not a date: ", date);
+        return "";
+    }
+    return getMonthName(date) + " " + date.getFullYear();
+}
+
+var getPrevMonthYear = function(date) {
+    if (!_.isDate(date)) {
+        console.log("getPrevMonthYear, parameter is not a date: ", date);
+        return null;
+    }
+    var prevMonth = date.getMonth() == 0 ? 11 : date.getMonth()-1;
+    var prevYear = date.getMonth() == 0 ? date.getFullYear()-1 : date.getFullYear();
+    var prevDate = new Date(date);
+    prevDate.setMonth(prevMonth);
+    prevDate.setFullYear(prevYear);
+    return prevDate;
+}
+
+var saveNewDateLabel = function(dateLabels, newDate) {
+    console.log("saveNewDateLabel, newDate=", newDate);
+    var newDateLabel = new DateLabel({
+        month: newDate,
+    });
+    DateLabel.update(
+        { month: newDateLabel.month },
+        { $setOnInsert: newDateLabel },
+        { upsert: true },
+        function(err, numAffected) {
+            if (err) {
+                console.log("Failed to save date labels");
             }
-        }
-    }
+            else {
+                dateLabels.push(newDateLabel);
 
-    if (!foundMatch) {
-        // optimization, don't repeately ask for old ids
-        obsoleteIds.push(data.id);
-    }
-}
-
-// for debugging only
-var saveJobDetail = function(fileName, data) {
-    console.log(data);
-    console.log("-----");
-    fs.appendFile(fileName, data, function(err) {
-        if (err) {
-            console.log("Failed to write to file: ", err);
-        }
-        else {
-            console.log("File ", fileName, " written");
-        }
-    });
-}
-
-// replace html coding with ascii character
-String.prototype.decodeHTML = function() {
-    var map = {"gt":">" /* , … */};
-    return this.replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, function($0, $1) {
-        if ($1[0] === "#") {
-            return String.fromCharCode($1[1].toLowerCase() === "x"
-                ? parseInt($1.substr(2), 16)
-                : parseInt($1.substr(1), 10));
-        } else {
-            return map.hasOwnProperty($1) ? map[$1] : $0;
-        }
-    });
-};
-
-
-var savePostToDatabase = function(post, topic, monthPosted) {
-    if (topic.title == "hiring") {
-        saveJobToDatabase(post, "Who Is Hiring", monthPosted);
-    }
-    else if (topic.title == "wantshired") {
-        saveCandidateToDatabase(post, "Who Wants To Be Hired", monthPosted);
-    }
-    else if (topic.title == "freelance" && post.text) {
-        var seekingFreelancerRe = /SEEKING FREELANCER/gi;
-        var seekingWorkRe = /SEEKING WORK/gi;
-        if (seekingFreelancerRe.test(post.text)) {
-            saveJobToDatabase(post, "Seeking Freelancer", monthPosted);
-        }
-        else if (seekingWorkRe.test(post.text)) {
-            saveCandidateToDatabase(post, "Seeking Freelance Work", monthPosted);
-        }
-        else {
-            console.log("freelance, post doesn't say what's seeking, ",
-                    post.by, ",", post.time, ",", post.id);
-        }
-    }
-}
-
-var saveCandidateToDatabase = function(candidate, source, monthPosted) {
-    var candidateDatum = new CandidateDatum({
-        id: candidate.id,
-        time: candidate.time * 1000,
-        source: source,
-        monthPosted: monthPosted,
-        description: candidate.text,
-    });
-
-    // only save if candidate.id is not present
-    CandidateDatum.update(
-            { id: candidateDatum.id },
-            { $setOnInsert: candidateDatum },
-            { upsert: true },
-            function(err, numAffected) {
-                if (err) {
-                    console.log("Failed to save a new candidate");
+                // kick out the oldest if new one has been added
+                // NOTE: whoishiring for the new month may have not been posted
+                // yet, but we remove the old one anyway, because frontend only
+                // shows one less months' data
+                if (dateLabels.length > 4) {
+                    dateLabels.shift();
+                }
+                else if (dateLabels.length < 4 || argv.f ) {
+                    // FIXME: if there are >= 4 date labels, even the actual
+                    // data is empty, we still only request the latest month,
+                    // maybe I should try
+                    // 1) check data by month and request if that
+                    // month is empty
+                    // 2) add a force flag and pass from command line, I think
+                    // this is a better solution
+                    var prevDate = getPrevMonthYear(newDateLabel.month);
+                    saveNewDateLabel(dateLabels, prevDate);
                 }
             }
-            );
+
+            // will still get the posts even datelabels failed to save
+            console.log("Getting " + newDate);
+            HnJobs.getWhoIsHiring('../data/whoishiring.txt', getMonthYearText(newDate));
+        });
 }
 
-var saveJobToDatabase = function(job, source, monthPosted) {
-    // don't show if the text is too short and there's no title
-    if ((!job.text || job.text.length < 16) && !job.title) {
-        return;
-    }
-    else {
-        if (!job.title) {
-            // this is the case for whoishiring posts
-            var title = job.text.split(/<[a-zA-Z]|\n/)[0];
-            var decodedTitle = title.decodeHTML();
-            job.title = decodedTitle.replace(/^[-\|\.\ ]+|[-\|\.\(\[\{\ ,;:]+$/g, '');
-            //console.log("job.title = " + job.title);
-            var titleLine = "job.title = " + job.title + "\n";
-        }
-        if (!job.text) {
-            job.text = job.title;
-        }
+var retrieve = function() {
+    var now = new Date();
+    var dateText = getMonthYearText(now);
+    var newDate = new Date(dateText);
+    DateLabel.find()
+        .sort("-month")
+        .exec(function(err, dateLabels) {
+            if (err) {
+                // still try to get new posts even datalabel fetch failed
+                console.log("Failed to find date labels, will not refresh jobs");
+            }
+            else {
+                var latestIndex = _.findIndex(dateLabels, function(o) {
+                    return o.month == newDate;
+                });
 
-        var jobType = ["Full Time"];
-        var parttimeRe = /part[\ \-]?time/gi;
-        if (parttimeRe.test(job.text)) {
-            jobType.push("Part Time");
-        }
-        var freelanceRe = /free[\ \-]?lanc/gi;
-        if (freelanceRe.test(job.text)) {
-            jobType.push("Freelance");
-        }
-
-        var remoteRe = /REMOTE/gi;
-        var remote = remoteRe.test(job.text);
-
-        var onsiteRe = /ONSITE/gi;
-        var onsite = onsiteRe.test(job.text);
-
-        var jobDatum = new JobDatum({
-            id: job.id,
-            time: job.time * 1000,
-            source: source,
-            company: "",
-            position: job.title,
-            description: job.text,
-            url: job.url,
-            monthPosted: monthPosted,
-            type: jobType,
-            where: "",
-            remote: remote,
-            onsite: onsite,
-        });
-
-        // only save if job.id is not present
-        JobDatum.update(
-                { id: jobDatum.id },
-                { $setOnInsert: jobDatum },
-                { upsert: true },
-                function(err, numAffected) {
-                    if (err) {
-                        console.log("Failed to save a new job");
+                if (latestIndex == -1) {
+                    console.log("lastestIndex==-1, new date appeared");
+                    saveNewDateLabel(dateLabels, newDate);
+                }
+                else if (latestIndex == 0) {
+                    console.log("lastestIndex==0, current date is the latest");
+                    // should only happen during developing
+                    if (dateLabels.length < 4) {
+                        var prevDate = getPrevMonthYear(newDate);
+                        saveNewDateLabel(dateLabels, prevDate);
                     }
                 }
-                );
-    }
-
-//    JobDatum.findOne(
-//            { id: jobDatum.id },
-//            function(err, existing) {
-//                if (!err) {
-//                    if (!existing || existing.time < jobDatum.time) {
-//                        existing = jobDatum;
-//                    }
-//                    existing.save(function(err) {
-//                        if (err) {
-//                            console.log("Failed to save a new job");
-//                        }
-//                    });
-//                }
-//            }
-//            );
-
-//    // only save if job.id is not present and timestamp is older
-//    // this won't work!
-//    JobDatum.update(
-//            { $and: [{id: jobDatum.id}, {time: {$lt: jobDatum.time}}] },
-//            { $setOnInsert: jobDatum},
-//            { upsert: true},
-//            function(err, numAffected) {
-//                if (err) {
-//                    console.log("Failed to save a new job");
-//                }
-//            }
-//            );
-
-}
-
-var getJobs = function(fileName) {
-    return requestById("", "jobstories", function(jobIds) {
-        //console.log(jobIds);
-        jobIds.forEach(function(jobId) {
-            requestById("item/", jobId, function(job) {
-                saveJobToDatabase(job);
-            });
-        });
-    });
-}
-
-// TODO: how to make this not global? save in db or something
-var obsoleteIds = [];
-
-var getWhoIsHiring = function(fileName, filter) {
-    return requestById("user/", "whoishiring", function(whoishiring) {
-        var postIds = whoishiring.submitted;
-        postIds.forEach(function(id) {
-            //console.log("id: ", id);
-            if (_.indexOf(obsoleteIds, id) == -1) {
-                requestById("item/", id, function(data) {
-                    parseWhosHiring(fileName, data, filter);
-                });
+                else {
+                    // latestIndex is not current month, shouldn't happen
+                    console.log("Current date: " + newDate +
+                            "is not the lastest saved in database: "
+                            + dateLabels[latestIndex].month);
+                }
             }
         });
-    });
 }
 
-module.exports.getJobs = getJobs;
-module.exports.getWhoIsHiring = getWhoIsHiring;
+if (argv.m == 'oneoff') {
+    retrieve();
+}
+else if (argv.m == 'daemon') {
+    var cronWhoIsHiring = new CronJob('00 03 21 * * *', function() {
+        retrieve();
+    },
+    start: true,
+    runOnInit: true,
+    timeZone);
+}
+
+//var cronGetJob = new CronJob('*/30 * * * * *', function() {
+//    HnJobs.getJobs('../data/jobs.txt');
+//}, null, true);
+//
 
---
+++ app/requester.js
@@ -0,0 +1,265 @@
+var https = require("https");
+var fs = require("fs");
+var _ = require('lodash');
+var JobDatum = require('./models/job');
+var CandidateDatum = require('./models/candidate');
+
+var requestById = function(type, id, onEnd) {
+    var options = {
+        hostname: "hacker-news.firebaseio.com",
+        path: "/v0/" + type + id + ".json?print=pretty",
+        method: "GET",
+        headers: { Accept: "text/html" }
+    };
+
+    var request = https.request(options,
+        function(response) {
+            //console.log("Server responded with status code: ", response.statusCode);
+            //console.log("Server responded with header: ", response.headers);
+            var body = "";
+            response.on("data", function(chunk) {
+                body += chunk;
+            });
+            response.on('end', function() {
+                var content = JSON.parse(body);
+                onEnd(content);
+            });
+        });
+    request.end();
+
+    request.on("error", function(e) {
+        console.error("request has error: " + e);
+    });
+}
+
+var topics = [
+    { title: "hiring"     , text: "Ask HN: Who is hiring.*" },
+    { title: "wantshired" , text: "Ask HN: Who wants to be hired.*" },
+    { title: "freelance"  , text: "Ask HN: Freelancer.*" },
+    ];
+
+var parseWhosHiring = function(fileName, data, filter) {
+    var foundMatch = false;
+    if (data.title) {
+        for (var i = 0; i < topics.length; ++i) {
+            var topic = topics[i];
+            var re = new RegExp(topic.text + filter, "gi");
+            if (re.test(data.title)) {
+                data.kids.forEach(function(entry) {
+                    //console.log("requesting item/" + entry);
+                    requestById("item/", entry, function(post) {
+                        if (post) {
+                            savePostToDatabase(post, topic, filter);
+                        }
+                    });
+                });
+                foundMatch = true;
+                break;
+            }
+        }
+    }
+
+    if (!foundMatch) {
+        // optimization, don't repeately ask for old ids
+        obsoleteIds.push(data.id);
+    }
+}
+
+// for debugging only
+var saveJobDetail = function(fileName, data) {
+    console.log(data);
+    console.log("-----");
+    fs.appendFile(fileName, data, function(err) {
+        if (err) {
+            console.log("Failed to write to file: ", err);
+        }
+        else {
+            console.log("File ", fileName, " written");
+        }
+    });
+}
+
+// replace html coding with ascii character
+String.prototype.decodeHTML = function() {
+    var map = {"gt":">" /* , … */};
+    return this.replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, function($0, $1) {
+        if ($1[0] === "#") {
+            return String.fromCharCode($1[1].toLowerCase() === "x"
+                ? parseInt($1.substr(2), 16)
+                : parseInt($1.substr(1), 10));
+        } else {
+            return map.hasOwnProperty($1) ? map[$1] : $0;
+        }
+    });
+};
+
+
+var savePostToDatabase = function(post, topic, monthPosted) {
+    if (topic.title == "hiring") {
+        saveJobToDatabase(post, "Who Is Hiring", monthPosted);
+    }
+    else if (topic.title == "wantshired") {
+        saveCandidateToDatabase(post, "Who Wants To Be Hired", monthPosted);
+    }
+    else if (topic.title == "freelance" && post.text) {
+        var seekingFreelancerRe = /SEEKING FREELANCER/gi;
+        var seekingWorkRe = /SEEKING WORK/gi;
+        if (seekingFreelancerRe.test(post.text)) {
+            saveJobToDatabase(post, "Seeking Freelancer", monthPosted);
+        }
+        else if (seekingWorkRe.test(post.text)) {
+            saveCandidateToDatabase(post, "Seeking Freelance Work", monthPosted);
+        }
+        else {
+            console.log("freelance, post doesn't say what's seeking, ",
+                    post.by, ",", post.time, ",", post.id);
+        }
+    }
+}
+
+var saveCandidateToDatabase = function(candidate, source, monthPosted) {
+    var candidateDatum = new CandidateDatum({
+        id: candidate.id,
+        time: candidate.time * 1000,
+        source: source,
+        monthPosted: monthPosted,
+        description: candidate.text,
+    });
+
+    // only save if candidate.id is not present
+    CandidateDatum.update(
+            { id: candidateDatum.id },
+            { $setOnInsert: candidateDatum },
+            { upsert: true },
+            function(err, numAffected) {
+                if (err) {
+                    console.log("Failed to save a new candidate");
+                }
+            }
+            );
+}
+
+var saveJobToDatabase = function(job, source, monthPosted) {
+    // don't show if the text is too short and there's no title
+    if ((!job.text || job.text.length < 16) && !job.title) {
+        return;
+    }
+    else {
+        if (!job.title) {
+            // this is the case for whoishiring posts
+            var title = job.text.split(/<[a-zA-Z]|\n/)[0];
+            var decodedTitle = title.decodeHTML();
+            job.title = decodedTitle.replace(/^[-\|\.\ ]+|[-\|\.\(\[\{\ ,;:]+$/g, '');
+            //console.log("job.title = " + job.title);
+            var titleLine = "job.title = " + job.title + "\n";
+        }
+        if (!job.text) {
+            job.text = job.title;
+        }
+
+        var jobType = ["Full Time"];
+        var parttimeRe = /part[\ \-]?time/gi;
+        if (parttimeRe.test(job.text)) {
+            jobType.push("Part Time");
+        }
+        var freelanceRe = /free[\ \-]?lanc/gi;
+        if (freelanceRe.test(job.text)) {
+            jobType.push("Freelance");
+        }
+
+        var remoteRe = /REMOTE/gi;
+        var remote = remoteRe.test(job.text);
+
+        var onsiteRe = /ONSITE/gi;
+        var onsite = onsiteRe.test(job.text);
+
+        var jobDatum = new JobDatum({
+            id: job.id,
+            time: job.time * 1000,
+            source: source,
+            company: "",
+            position: job.title,
+            description: job.text,
+            url: job.url,
+            monthPosted: monthPosted,
+            type: jobType,
+            where: "",
+            remote: remote,
+            onsite: onsite,
+        });
+
+        // only save if job.id is not present
+        JobDatum.update(
+                { id: jobDatum.id },
+                { $setOnInsert: jobDatum },
+                { upsert: true },
+                function(err, numAffected) {
+                    if (err) {
+                        console.log("Failed to save a new job");
+                    }
+                }
+                );
+    }
+
+//    JobDatum.findOne(
+//            { id: jobDatum.id },
+//            function(err, existing) {
+//                if (!err) {
+//                    if (!existing || existing.time < jobDatum.time) {
+//                        existing = jobDatum;
+//                    }
+//                    existing.save(function(err) {
+//                        if (err) {
+//                            console.log("Failed to save a new job");
+//                        }
+//                    });
+//                }
+//            }
+//            );
+
+//    // only save if job.id is not present and timestamp is older
+//    // this won't work!
+//    JobDatum.update(
+//            { $and: [{id: jobDatum.id}, {time: {$lt: jobDatum.time}}] },
+//            { $setOnInsert: jobDatum},
+//            { upsert: true},
+//            function(err, numAffected) {
+//                if (err) {
+//                    console.log("Failed to save a new job");
+//                }
+//            }
+//            );
+
+}
+
+var getJobs = function(fileName) {
+    return requestById("", "jobstories", function(jobIds) {
+        //console.log(jobIds);
+        jobIds.forEach(function(jobId) {
+            requestById("item/", jobId, function(job) {
+                saveJobToDatabase(job);
+            });
+        });
+    });
+}
+
+// TODO: how to make this not global? save in db or something
+var obsoleteIds = [];
+
+var getWhoIsHiring = function(fileName, filter) {
+    return requestById("user/", "whoishiring", function(whoishiring) {
+        var postIds = whoishiring.submitted;
+        postIds.forEach(function(id) {
+            //console.log("id: ", id);
+            if (_.indexOf(obsoleteIds, id) == -1) {
+                requestById("item/", id, function(data) {
+                    parseWhosHiring(fileName, data, filter);
+                });
+            }
+        });
+    });
+}
+
+module.exports.getJobs = getJobs;
+module.exports.getWhoIsHiring = getWhoIsHiring;
+
