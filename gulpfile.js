8d091c90440181fcc8fce678a524362c452a5a48
Shan Wang <uniwangshan@gmail.com>
2016-02-25 02:49:10 +0000

    Return stream in gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,21 @@ var ngConstant = require('gulp-ng-constant');
 // when this runs in the backend, process.env.NODE_ENV is known
 gulp.task('config', function() {
     var environment = process.env.NODE_ENV || 'development';
-    gulp.src('public/config/' + environment + '.json')
+    var stream = gulp.src('public/config/' + environment + '.json')
         .pipe(ngConstant({
             name: 'app.config'
         }))
         .pipe(concat('public/js/config.js'))
         .pipe(gulp.dest('.'))
         .on('error', function() { });
+    return stream;
 });
 
 gulp.task('build', ['config'], function() {
-    gulp.src(['public/js/config.js', 'public/js/*.js'])
+    var stream = gulp.src(['public/js/config.js', 'public/js/*.js'])
         .pipe(concat('app.js'))
         .pipe(gulp.dest('public/dist'));
+    return stream;
 });
 
 gulp.task('watch:build', ['build'], function() {
